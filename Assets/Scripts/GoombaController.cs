62d62560081103142d3fd415fbf60a019bfd519c
sponge <sponge@d8d.org>
2016-04-19 16:29:13 -0500

    get goombas walking, move assets around

---
+++ Assets/Scripts/GoombaController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
+
+public class GoombaController : MonoBehaviour {
+
+    private CharacterController2D controller;
+
+    public float gravity;
+    public float speed;
+
+    private bool flipSpeed;
+
+	void Start () {
+        controller = GetComponent<CharacterController2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var vel = controller.velocity;
+
+        if (controller.collisionState.left || controller.collisionState.right)
+        {
+            flipSpeed = !flipSpeed;
+        }
+
+        vel.x = speed * (flipSpeed ? -1 : 1);
+        vel.y -= 625 * Time.deltaTime;
+
+        var oldLayer = this.gameObject.layer;
+        gameObject.layer = 31;
+        controller.move(vel * Time.deltaTime);
+        gameObject.layer = oldLayer;
+    }
+}
