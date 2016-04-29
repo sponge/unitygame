8d8f5573fa9f0f3bc0dbddbee0d896018091e3b3
sponge <sponge@d8d.org>
2016-04-29 12:40:40 -0500

    cleanup, second map, gamesession object, goal object

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameSession : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this);
+        SceneManager.LoadScene("Levels/smw");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- maps/tilesets/smw.tsx
+++ maps/tilesets/smw.tsx
@@ -1,6 +1,110 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="smw" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="2352">
+<tileset name="smw" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="2352" columns="48">
  <image source="smw.png" width="817" height="837"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="prefab" value="Assets/GameObjects/CoinBlock.prefab"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="prefab" value="Assets/GameObjects/Coin.prefab"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="144">
+  <properties>
+   <property name="platform" value="true"/>
+   <property name="solid" value="false"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0">
+    <polyline points="0,0 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="145">
+  <properties>
+   <property name="platform" value="true"/>
+   <property name="solid" value="false"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0">
+    <polyline points="0,0 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="146">
+  <properties>
+   <property name="platform" value="true"/>
+   <property name="solid" value="false"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0">
+    <polyline points="0,0 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="151">
+  <properties>
+   <property name="platform" value="true"/>
+   <property name="solid" value="false"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0">
+    <polyline points="0,0 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="152">
+  <properties>
+   <property name="platform" value="true"/>
+   <property name="solid" value="false"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0">
+    <polyline points="0,0 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="153">
+  <properties>
+   <property name="platform" value="true"/>
+   <property name="solid" value="false"/>
+  </properties>
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0">
+    <polyline points="0,0 16,0"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="154">
   <objectgroup draworder="index">
    <object id="0" x="0" y="0" width="16" height="16"/>
@@ -10,5 +114,110 @@
   <objectgroup draworder="index">
    <object id="0" x="0" y="0" width="16" height="16"/>
   </objectgroup>
+ </tile>
+ <tile id="156">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="199">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="200">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="201">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="202">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="203">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="204">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="247">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="248">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="249">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="250">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="251">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="252">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="295">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="296">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="297">
+  <properties>
+   <property name="solid" value="false"/>
+  </properties>
+ </tile>
+ <tile id="298">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="299">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="300">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1645">
+  <properties>
+   <property name="prefab" value="Assets/GameObjects/RedCoin.prefab"/>
+  </properties>
+ </tile>
+ <tile id="1649">
+  <properties>
+   <property name="prefab" value="Assets/GameObjects/Goomba.prefab"/>
+  </properties>
  </tile>
 </tileset>
